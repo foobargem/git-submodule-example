1d753344d000fc7f24d82ee93d9cc2822fde7f47
eunchul <agfe09@gmail.com>
2011-01-15 17:02:30 +0900

    import wemakecode proejct

---
+++ wemakecode/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wemakecode::Application.config.secret_token = '96ee28bdc4f45a0b044618651996ced016377d252be7decbd15648b9cf55ac161ed46b81a54a0bed35d2f67846c5256394a8684ad239a9046fef3cca113e8d48'
